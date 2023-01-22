7e9280feec29bfc2154aed0407f75d3daddff31f
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-01-22 22:36:08 +0900

    feat: default component

---
+++ components/default/Component.tsx
@@ -0,0 +1,12 @@
+import classNames from "classnames/bind";
+import styles from "./Component.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface Props {}
+
+const Component = ({}: Props) => {
+  return <div></div>;
+};
+
+export default Component;
