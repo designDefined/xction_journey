9ab30719d682e88a679765a31ac2f22835b83893
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-02-03 17:59:59 +0900

    fix:last link

--- pages/1/2/crossframe/index.tsx
+++ pages/1/2/crossframe/index.tsx
@@ -28,7 +28,8 @@ const Index = ({}: Props) => {
       <div
         className={cx("andNext")}
         onClick={() => {
-          location.href = "https://naver.com";
+          location.href =
+            "https://www.notion.so/Chapter-1-cdd2d6e13081475e8bbf703a48f90e0c#c56c15af37894f3e93347a17974c8468";
         }}
       >
         다음 챕터로
